565a0574d4f817ebb1681cf24ead7cf1cbdfa1cc
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-10 17:38:15 +0000

    Release 1.0.17

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -8,6 +8,6 @@ import java.lang.String;
 
 public final class App {
   public static void main(String[] args) {
-    // import com.mavenagi.api.MavenAGI
+    // import com.mavenagi.AsyncMavenAGI
   }
 }
